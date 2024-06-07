8adf1e55e9de3b82d1dabb98a88b42b279cec419
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2024-06-07 21:22:48 +0300

    update front-end jun-7

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
-window.addEventListener("scroll",function(){
-    var nav = document.getElementById("navbar");
-    nav.classList.toggle("sticky", window.scrollY > 40 );
-})
+var path = window.location.pathname;
+var nav = document.getElementById("navbar");
+
+if(path=="/"){
+    window.addEventListener("scroll",function(){            
+        nav.classList.toggle("sticky", window.scrollY > 40 );
+    })
+}
+else{
+    nav.classList.add( "sticky");
+}
